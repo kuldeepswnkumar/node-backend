278757019df9a9318115863ec7757f15921aeea0
KuldeepGupta <kuldeepswnkumar@gmail.com>
2024-03-19 01:43:18 +0530

    utils code config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from "./dbconnect/dbconn.js";
-
+import express from "express";
+const app = express();
 
 dotenv.config({path:'./env'})
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running of Port ${process.env.PORT}`);
+    })
+})
+.catch((err) => console.log("Database Connection Failed!!", err))
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+
+// First Method
+const asyncHandler = (requestHandler) =>{
+    (req, res, next)=>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+    }
+}
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (func) => () =>{}
+// const asyncHandler = (func) => async() =>{}
+
+
+// Second Method
+// const asyncHandler = (func) => async(req, res, next) =>{
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+ 
+export {asyncHandler}
